294180b916ce33729385e20e424d825c44878c89
Royal <royal.21ai040@sode-edu.in>
2025-02-19 13:45:36 +0530

    fix: update next config to js format

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  // Add any other config options you need
 };
 
 module.exports = nextConfig;
